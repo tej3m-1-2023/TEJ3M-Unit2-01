78641ede12f3909ad4a5014099cdd8ea333fa7af
tej3m-1-2023 <144251866+tej3m-1-2023@users.noreply.github.com>
2023-09-19 10:56:52 -0400

    Create main2.ino

---
+++ main2.ino
@@ -0,0 +1,17 @@
+// Created by: Mr Coxall
+// Created on: Sept 2023
+//
+// This is the blink program
+
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
